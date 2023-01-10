84a5558957bfaaf4bc08a1783018161aae905bd9
yhao3 <kyrieeeee2@gmail.com>
2023-01-10 17:11:02 +0800

    Initial commit.

---
+++ src/main/java/org/yhao3/crudbestpractice/CrudBestPracticeApplication.java
@@ -0,0 +1,15 @@
+package org.yhao3.crudbestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yhao3
+ */
+@SpringBootApplication
+public class CrudBestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBestPracticeApplication.class, args);
+	}
+}
