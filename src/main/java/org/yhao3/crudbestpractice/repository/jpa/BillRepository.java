9ba66c71a173a213ba8554164013264328deb4de
yhao3 <kyrieeeee2@gmail.com>
2023-01-17 16:07:54 +0800

    feat: Resolve Hibernate L2C 1 + 1 problem.

--- src/main/java/org/yhao3/crudbestpractice/repository/jpa/BillRepository.java
+++ src/main/java/org/yhao3/crudbestpractice/repository/jpa/BillRepository.java
@@ -1,9 +1,21 @@
 package org.yhao3.crudbestpractice.repository.jpa;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import org.yhao3.crudbestpractice.domain.Bill;
 
+import java.util.Optional;
+
 @Repository(value = "billRepository")
 public interface BillRepository extends JpaRepository<Bill, Long> {
+
+    /**
+     * Find bill by id.
+     * @param id must not be {@literal null}.
+     * @return the entity.
+     */
+    @EntityGraph(attributePaths = { "billDetails" })
+    Optional<Bill> findById(@NonNull Long id);
 }
