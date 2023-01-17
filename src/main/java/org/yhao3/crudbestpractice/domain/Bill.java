56211c6e7e7cb90540c6bbc78bd8f5f915b889f3
yhao3 <kyrieeeee2@gmail.com>
2023-01-17 16:03:15 +0800

    refactor: Update Bill.java

--- src/main/java/org/yhao3/crudbestpractice/domain/Bill.java
+++ src/main/java/org/yhao3/crudbestpractice/domain/Bill.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -22,6 +23,7 @@ import java.util.Set;
 @Table(name = "bill")
 public class Bill implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
